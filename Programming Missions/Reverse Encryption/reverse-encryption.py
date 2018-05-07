c4b2b601e5fb314c53f9e6ca60fc16d2ba8f2ea8
Semnodime <Semnodime@users.noreply.github.com>
2018-05-07 04:39:23 +0200

    Init Level Description and Examples

---
+++ Programming Missions/Reverse Encryption/reverse-encryption.py
@@ -0,0 +1,20 @@
+"""
+Level 3
+This level is about reversing an encryption algorithm.
+
+The original text is a bunch of fake serial numbers.
+Look into ./serials_example.txt to see some examples of what they look like.
+The PHP-code which is used for the encryption is given in ./crypter.php:
+
+This is a example of the encrypted text:
+(Ignore newlines and ''' delimiters)
+'''
+-192 -216 -187 -174 -163 -147 -190 -203 -110 -173 -170 -208 -148 -185 -187 -172 -174 -225 -181 -265 -134 -170 -160 -156
+-158 -146 -146 -189 -131 -125 -147 -170 -158 -139 -139 -193 -180 -181 -185 -225 -176 -176 -146 -149 -197 -55 -214 -115
+-154 -209 -118 -170 -172 -135 -204 -165 -158 -154 -188 -239 -147 -186 -111 -192 -154 -126 -130 -194 -127 -184 -191 -145
+-232 -146 -201 -189 -175 -192 -182 -196 -145 -150 -160 -150 -182 -169 -187 -188 -182 -118 -166 -189 -74 -93 -191 -178
+-209 -200 -153 -241
+'''
+
+The last one of the serial numbers you decrypted is the answer.
+"""
